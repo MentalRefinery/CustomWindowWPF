9e52f279227c63d19240f48cc36ea6b72a19d42a
Mental Refinery <mentalrefinery@gmail.com>
2020-03-10 14:17:23 +0200

    WindowViewModel

--- CustomWindow/MainWindow.xaml.cs
+++ CustomWindow/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CustomWindow.ViewModel;
+
 
 namespace CustomWindow
 {
@@ -23,6 +25,8 @@ namespace CustomWindow
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new WindowViewModel(this);
         }
     }
 }
---
+++ CustomWindow/ViewModel/WindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+
+namespace CustomWindow.ViewModel
+{
+    /// <summary>
+    /// View model for the Custom Flat Window
+    /// </summary>
+     public class WindowViewModel: BaseViewModel
+     {
+        #region Primate Member
+        private Window mWindow;
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        private int mOuterMarginSize = 20;
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        private int mWindowRadius = 10;
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// The size of the resize border around the window
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
+
+        /// <summary>
+        /// Size of the resize border around the window, taking into account the outer margin.
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+       
+
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+
+            //Listen out for the window resizing
+            mWindow.StateChanged += (sender, e) =>
+            {
+                //Fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+        }
+
+       
+        #endregion
+    }
+}
